01a2f98c0afcbde292aa23a7249c4208dae5d920
ADi Hendro <patrick_segara@students.itb.ac.id>
2019-11-08 23:38:14 +0700

    Chessboard + main

---
+++ chessbodoh.c
@@ -0,0 +1,17 @@
+//Mencoba cara bodoh ngeprint chess board
+#include <stdio.h>
+int main (){
+    printf("         A       B       C       D       E       F       G      H    \n");
+    printf("     _________________________________________________________________\n");
+    for (int i=1;i<=8;i++){
+        printf("     |       |       |       |       |       |       |       |       |\n");
+        printf("  %d  |  A,%d  |  B,%d  |  C,%d  |  D,%d  |  E,%d  |  F,%d  |  G,%d  |  H,%d  |\n",i,i,i,i,i,i,i,i,i);
+        printf("     |_______|_______|_______|_______|_______|_______|_______|_______|\n");
+    } 
+    
+
+
+
+
+    return 0;
+}
