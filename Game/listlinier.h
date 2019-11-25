5e619344ea8a3d4e4ef6cac5078f267dd958ddc2
shanelbert <57350807+shanelbert@users.noreply.github.com>
2019-11-25 21:50:47 +0700

    Komen printinfo() diubah dikit

--- Game/listlinier.h
+++ Game/listlinier.h
@@ -74,11 +74,8 @@ int NbElmtList (List L);
 /* Mengirimkan banyaknya elemen list; mengirimkan 0 jika list kosong */
 
 void PrintInfo (List L);
-/* I.S. List mungkin kosong */
-/* F.S. Jika list tidak kosong, isi list dicetak ke kanan: [e1,e2,...,en] */
-/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
-/* Jika list kosong : menulis [] */
-/* Tidak ada tambahan karakter apa pun di awal, akhir, atau di tengah */
+/* I.S. List tidak kosong */
+/* F.S. Jika list tidak kosong, list akan dicetak dengan format yang ditentukan. Contoh: 1. (a, 1) 
 
 void DelList(List* L);
 /* I.S. List mungkin kosong */
